2f9dccc2c272adf52beae947cfb5affd350325ee
Philip Laine <philip.laine@gmail.com>
2020-07-13 21:03:03 +0200

    Replace interna message struct with imported from flux

--- pkg/api/events.go
+++ pkg/api/events.go
@@ -2,11 +2,13 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
+	"github.com/fluxcd/flux/pkg/event"
+
 	"github.com/xenitab/flux-status/pkg/exporter"
-	"github.com/xenitab/flux-status/pkg/flux"
 )
 
 func (s *Server) HandleEvents(w http.ResponseWriter, r *http.Request) {
@@ -16,14 +18,19 @@ func (s *Server) HandleEvents(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 500)
 		return
 	}
-	fluxEvent := flux.Event{}
+	fluxEvent := event.Event{}
 	if err := json.Unmarshal(body, &fluxEvent); err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
 	// Send exporter event
-	event := convertToEvent(fluxEvent)
+	event, err := convertToEvent(fluxEvent)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
 	if err := s.Exporter.Send(event); err != nil {
 		s.Log.Error(err, "Could not send event through exporter")
 		http.Error(w, err.Error(), 500)
@@ -44,23 +51,28 @@ func (s *Server) HandleEvents(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 }
 
-func convertToEvent(e flux.Event) exporter.Event {
-	commitId := e.Metadata.Commits[0].Revision
+func convertToEvent(e event.Event) (exporter.Event, error) {
+	if e.Metadata.Type() != event.EventSync {
+		return exporter.Event{}, fmt.Errorf("Could not parse event metatada type: %v", e.Metadata.Type())
+	}
+
+	syncMetadata := e.Metadata.(*event.SyncEventMetadata)
+	commitId := syncMetadata.Commits[0].Revision
 
 	var message string
 	var state exporter.EventState
-	if len(e.Metadata.Errors) == 0 {
+	if len(syncMetadata.Errors) == 0 {
 		state = exporter.EventStateSucceeded
 		message = "Succeeded"
 	} else {
 		state = exporter.EventStateFailed
 		message = "Errors:"
-		for _, err := range e.Metadata.Errors {
-			message = message + err.Id + ","
+		for _, err := range syncMetadata.Errors {
+			message = message + err.ID.String() + ","
 		}
 	}
 
-	return exporter.Event{
+	event := exporter.Event{
 		Id:       "flux-status",
 		Instance: "dev",
 		Event:    "sync",
@@ -68,4 +80,6 @@ func convertToEvent(e flux.Event) exporter.Event {
 		CommitId: commitId,
 		State:    state,
 	}
+
+	return event, nil
 }
